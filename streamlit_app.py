f17f9d65ad597830cfd7f7df315863d760b0a6e7
ssingh-hash <ssingh@shelterinsurance.com>
2025-03-11 16:24:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + " "
+        ingredients_string += fruit_chosen + ' '
         
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
@@ -50,7 +50,7 @@ if ingredients_list:
         
         st.write(ingredients_string) 
     time_to_insert = st.button('submit order')
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name)
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
         values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
 
     st.write("Inserted")
