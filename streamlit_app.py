3d6372c430f1e29d75ded45035e481f667362a5d
ssingh-hash <ssingh@shelterinsurance.com>
2025-03-11 15:56:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,30 +35,27 @@ ingredients_list = st.multiselect (
 )
 
 if ingredients_list:
-  
-    
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
-       search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-       #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-       st.subheader(fruit_chosen + 'Nutrition Information')
-       
-       smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
-       sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-
-   #st.write(ingredients_string) 
-
-   my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-            values ('""" + ingredients_string + """')"""
-
-
-
-time_to_insert = st.button('submit order')
-if ingredients_string:
-    session.sql(my_insert_stmt).collect()
-    st.success('Your Smoothie is ordered!', icon="✅")
+        
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        
+        #st.write(ingredients_string) 
+    
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
+        values ('""" + ingredients_string + """')"""
+    
+    time_to_insert = st.button('submit order')
+    
+    if ingredients_string:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✅")
 
 
 
