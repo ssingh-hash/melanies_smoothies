75d5a141f1d2c4605c6742828276321bc0245f59
ssingh-hash <ssingh@shelterinsurance.com>
2025-03-11 15:58:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,10 +48,13 @@ if ingredients_list:
         
         #st.write(ingredients_string) 
     
-my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-    values ('""" + ingredients_string + """')"""
+# my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
+#     values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
 
 time_to_insert = st.button('submit order')
+
+my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
+    values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
 
 if ingredients_string:
     session.sql(my_insert_stmt).collect()
