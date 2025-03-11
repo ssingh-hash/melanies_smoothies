5840b231d63d1210510d796c51373623d6243994
ssingh-hash <ssingh@shelterinsurance.com>
2025-03-11 11:42:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 #converting snowflake dataframe in to pandas dataframe
-pd_df=mydataframe.to_pandas()
+pd_df=my_dataframe.to_pandas()
 #st.dataframe(pd_df)
 #st.stop()
 
