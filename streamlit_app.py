a2c6fa45160a452a6adc215ac4f3fdf7f25da179
ssingh-hash <ssingh@shelterinsurance.com>
2025-03-11 15:51:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,25 +7,6 @@ import requests
 
 from snowflake.snowpark.context import get_active_session
 
-helpful_links = [
-    "https://docs.streamlit.io",
-    "https://docs.snowflake.com/en/developer-guide/streamlit/about-streamlit",
-    "https://github.com/Snowflake-Labs/snowflake-demo-streamlit",
-    "https://docs.snowflake.com/en/release-notes/streamlit-in-snowflake"
-]
-
-# Write directly to the app
-st.title("Example Streamlit App :balloon:")
-st.write(
-    f"""Replace the code in this example app with your own code! And if you're new to Streamlit, here are some helpful links:
-
-    • :page_with_curl: [Streamlit open source documentation]({helpful_links[0]})
-    • :snow: [Streamlit in Snowflake documentation]({helpful_links[1]}) 
-    • :books: [Demo repo with templates]({helpful_links[2]})
-    • :memo: [Streamlit in Snowflake release notes]({helpful_links[3]})
-    """
-)
-
 st.title(":cup_with_straw: Customise your Smooothie :cup_with_straw:")
 st.write(
     f""" Choose the fruits you want in your custom smoothie """ 
@@ -34,7 +15,7 @@ st.write(
 import streamlit as st
 
 name_on_order = st.text_input("name on smoothie")
-st.write("The name on your smoothie will be :", name_on_order)
+st.write(f"The name on your smoothie will be :, {name_on_order}")
 
 cnx = st.connection ("snowflake")
 session = cnx.session()
@@ -76,10 +57,8 @@ if ingredients_list:
 
 
  
-  
-   time_to_insert = st.button('submit order')
-
-   if ingredients_string:
+  time_to_insert = st.button('submit order')
+  if ingredients_string:
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered!', icon="✅")
 
