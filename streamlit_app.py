95b5bf6c5ddcc911599e66cb4ac41294db678be6
ssingh-hash <ssingh@shelterinsurance.com>
2025-03-11 11:41:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ cnx = st.connection ("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('search_on'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+#st.stop()
 #converting snowflake dataframe in to pandas dataframe
 pd_df=mydataframe.to_pandas()
 #st.dataframe(pd_df)
